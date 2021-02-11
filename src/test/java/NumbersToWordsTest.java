ac2f7f25259186576c8640af9a5e25ac933ebcc0
Elizaveta Komarova <lisakomarova20000@gmail.com>
2021-02-11 21:31:18 +0300

    added dependencies logger instead of System.out.print checking for empty string with isEmpty method removed warnings where possible removed redundant code added separate files for classes throws exception when number has no exponent in dictionary

--- src/test/java/NumbersToWordsTest.java
+++ src/test/java/NumbersToWordsTest.java
@@ -1,3 +1,6 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
@@ -8,15 +11,21 @@ import org.junit.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class NumbersToWordsTest {
 
+    final Logger logger = LogManager.getLogger(NumbersToWordsTest.class.getName());
+
     NumbersToWords ntw = new NumbersToWords();
-    NumbersToWords.AbstractProcessor processor = new NumbersToWords.DefaultProcessor();
+    final DefaultProcessor processor = new DefaultProcessor();
+
 
     public NumbersToWordsTest() throws IOException {
+        BasicConfigurator.configure();
     }
 
     @Test
@@ -25,30 +34,54 @@ public class NumbersToWordsTest {
         InputStream in = new FileInputStream("src/test/resources/ExcelNumbers.xls");
         HSSFWorkbook wb = new HSSFWorkbook(in);
 
-        long inNumber = 0;
+        BigInteger inNumber = new BigInteger(String.valueOf(0));
         String inString = null;
 
         Sheet sheet = wb.getSheetAt(0);
+        String equation = "";
         for (Row row : sheet) {
             for (Cell cell : row) {
                 CellType cellType = cell.getCellType();
 
                 switch (cellType) {
                     case NUMERIC:
-                        System.out.print((inNumber = (long) cell.getNumericCellValue()) + " = ");
+                        equation = (inNumber = BigDecimal.valueOf(cell.getNumericCellValue()).toBigInteger()) + " = ";
                         break;
 
                     case STRING:
-                        System.out.print((inString = cell.getStringCellValue()));
+                        equation += (inString = cell.getStringCellValue());
                         break;
 
                     default:
                         break;
                 }
             }
-            System.out.println();
-            assertEquals(inString, processor.getName(inNumber));
+            logger.info(equation);
+            assertEquals(inString, processor.getName(new BigInteger(String.valueOf(inNumber))));
         }
     }
 
+    @Test
+    public void testGetNameAllTableError() throws Exception {
+
+        InputStream in = new FileInputStream("src/test/resources/ErrorExcelNumbers.xls");
+        HSSFWorkbook wb = new HSSFWorkbook(in);
+
+        BigInteger inNumber = new BigInteger(String.valueOf(0));
+
+        Sheet sheet = wb.getSheetAt(0);
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                CellType cellType = cell.getCellType();
+
+                if (cellType == CellType.NUMERIC) {
+                    inNumber = BigDecimal.valueOf(cell.getNumericCellValue()).toBigInteger();
+                }
+            }
+        }
+        final BigInteger finalInNumber = inNumber;
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> processor.getName(new BigInteger(String.valueOf(finalInNumber))));
+        assertNotNull(thrown.getMessage());
+        logger.error(thrown);
+    }
 }
